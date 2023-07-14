521eae084e3254fc08cc4e65494e83b26f8a460b
白雪鹏 <baixuepeng@jylink.com>
2023-07-14 12:43:17 +0800

    feat: modify vite config

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,15 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
+import { createViteConfig } from '@config/vite'
+import { defineConfig, UserConfig } from 'vite'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
+  return await createViteConfig(command, mode, process.cwd())
 })
---
+++ configs/vite/src/utils/index.ts
@@ -0,0 +1,61 @@
+import type { ProxyOptions } from 'vite'
+
+export interface ViteEnv {
+  VITE_USE_MOCK: boolean
+  VITE_USE_PWA: boolean
+  VITE_PUBLIC_PATH: string
+  VITE_PROXY: [string, string][]
+  VITE_GLOB_APP_TITLE: string
+  VITE_GLOB_APP_SHORT_NAME: string
+  VITE_USE_CDN: boolean
+  VITE_DROP_CONSOLE: boolean
+  VITE_USE_HTTPS: boolean
+  VITE_BUILD_COMPRESS: 'gzip' | 'brotli' | 'none'
+  VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean
+  VITE_LEGACY: boolean
+  VITE_USE_IMAGEMIN: boolean
+}
+
+export function wrapperEnv(envConf: Record<string, any>): ViteEnv {
+  const viteEnv: Partial<ViteEnv> = {}
+
+  for (const key of Object.keys(envConf)) {
+    let realName = envConf[key].replace(/\\n/g, '\n')
+    realName =
+      realName === 'true' ? true : realName === 'false' ? false : realName
+
+    if (key === 'VITE_PROXY' && realName) {
+      try {
+        realName = JSON.parse(realName.replace(/'/g, '"'))
+      } catch (error) {
+        realName = ''
+      }
+    }
+
+    viteEnv[key] = realName
+    if (typeof realName === 'string') {
+      process.env[key] = realName
+    } else if (typeof realName === 'object') {
+      process.env[key] = JSON.stringify(realName)
+    }
+  }
+  return viteEnv as ViteEnv
+}
+
+export function resolveProxy(
+  proxyList: [string, string][] = [],
+): Record<string, ProxyOptions> {
+  const proxy: Record<string, ProxyOptions> = {}
+  for (const [prefix, target] of proxyList) {
+    const isHttps = /^https:\/\//.test(target)
+    // https://github.com/http-party/node-http-proxy#options
+    proxy[prefix] = {
+      target,
+      changeOrigin: true,
+      ws: true,
+      rewrite: (path) => path.replace(new RegExp(`^${prefix}`), ''),
+      ...(isHttps ? { secure: false } : {}),
+    }
+  }
+  return proxy
+}
